1891bf88f627b081edb906a15490f5a33a61b66b
fx67ll <2717745014@qq.com>
2023-03-17 14:21:20 +0800

    format 404 error page

--- src/router/index.js
+++ src/router/index.js
@@ -31,7 +31,7 @@ export const fx67llRoutes = [{
 ]
 
 const router = new Router({
-	mode: 'hash', // history模式，去掉url中的#
+	mode: 'history', // history模式，去掉url中的#
 	scrollBehavior: () => ({
 		y: 0
 	}),
--- src/views/index-404.vue
+++ src/views/index-404.vue
@@ -1,30 +1,31 @@
 <template>
 	<div class="fx67ll-404">
 		<nav class="shelf">
-			<a class="book home-page">Home page</a>
-			<a class="book about-us">About us</a>
-			<a class="book contact">Contact</a>
-			<a class="book faq">F.A.Q.</a>
+			<a class="book home-page" href="https://fx67ll.com" target="_blank">fx67ll.com</a>
+			<a class="book about-us" href="https://nav.fx67ll.com" target="_blank">ABOUT ME</a>
+			<a class="book contact" href="https://fx67ll.xyz" target="_blank">fx67ll.xyz</a>
+			<a class="book faq" href="https://github.com/fx67ll" target="_blank">F.A.Q.</a>
 			<span class="book not-found"></span>
 			<span class="door left"></span>
 			<span class="door right"></span>
 		</nav>
 		<h1>Error 404</h1>
-		<!-- <p>糟糕，您要找的网页消失了，请联系管理员充值续费。。。</p>
+		<!-- for fx67ll -->
+		<p>糟糕，您要找的网页消失了，请联系管理员充值续费。。。</p>
 		<div class="fx67ll-footer">
 			Designed & Powered by
 			<a href="https://fx67ll.com" target="_blank">fx67ll</a>
 			&#12288; Copyright© 2018-{{ year }} &#12288;
 			<a href="https://beian.miit.gov.cn/#/Integrated/index" target="_blank">皖ICP备18017174号</a>
-		</div> -->
+		</div>
 		<!-- for lubenwei -->
-		<p>很抱歉，网页正在按照备案整改要求整改，敬请期待。。。</p>
+		<!-- <p>很抱歉，网页正在按照备案整改要求整改，敬请期待。。。</p>
 		<div class="fx67ll-footer">
 			Designed & Powered by
 			<a href="" target="_blank">zichengc</a>
 			&#12288; Copyright© 2018-{{ year }} &#12288;
 			<a href="https://beian.miit.gov.cn/#/Integrated/index" target="_blank">苏ICP备2021005710号</a>
-		</div>
+		</div> -->
 	</div>
 </template>
 
